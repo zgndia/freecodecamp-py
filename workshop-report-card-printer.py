1687231082c8e20d1f6b9ae534008198d21e4a4f
Zgn <ozgunkerimgun@hotmail.com>
2026-06-26 13:37:02 +0300

    Initial commit

---
+++ workshop-report-card-printer.py
@@ -0,0 +1,12 @@
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score, type(score))
